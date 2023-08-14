4d3890c2714e96cf80b0b007b3034a208b252ab8
pagh2322 <pagh2322@naver.com>
2023-08-14 21:06:09 +0900

    Init: 프로젝트 생성
    프로젝트를 생성했어요.
    
    Resolves: #1

---
+++ honeycourses/honeycourses/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  honeycourses
+//
+//  Created by pagh on 2023/08/14.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+	override func viewDidLoad() {
+		super.viewDidLoad()
+		// Do any additional setup after loading the view.
+	}
+
+
+}
+
---
+++ honeycourses/honeycoursesUITests/honeycoursesUITestsLaunchTests.swift
@@ -0,0 +1,32 @@
+//
+//  honeycoursesUITestsLaunchTests.swift
+//  honeycoursesUITests
+//
+//  Created by pagh on 2023/08/14.
+//
+
+import XCTest
+
+final class honeycoursesUITestsLaunchTests: XCTestCase {
+
+    override class var runsForEachTargetApplicationUIConfiguration: Bool {
+        true
+    }
+
+    override func setUpWithError() throws {
+        continueAfterFailure = false
+    }
+
+    func testLaunch() throws {
+        let app = XCUIApplication()
+        app.launch()
+
+        // Insert steps here to perform after app launch but before taking a screenshot,
+        // such as logging into a test account or navigating somewhere in the app
+
+        let attachment = XCTAttachment(screenshot: app.screenshot())
+        attachment.name = "Launch Screen"
+        attachment.lifetime = .keepAlways
+        add(attachment)
+    }
+}
